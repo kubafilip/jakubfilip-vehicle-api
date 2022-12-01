4509243204c886e51da84141c07ea5d2a9a8d892
Jakub Filip <kubafilip123@gmail.com>
2022-12-02 00:28:48 +0100

    Hello World with Spring

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # jakubfilip-vehicle-api
+
+build project from terminal:
+`mvn clean install`
+
+run project from terminal:
+`mvn spring-boot:run`
---
+++ src/test/java/com/jakub_filip/vehicle/JakubfilipVehicleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakub_filip.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakubfilipVehicleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
